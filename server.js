56f6e982099c8a935fd41e99dc922a31c8e0660a
disururathnayake <disuru.office@gmail.com>
2025-04-25 15:01:43 +1000

    setup cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["http://localhost:3000", "https://quick-converter-five.vercel.app"]
 }));
 
 const upload = multer({ dest: "uploads/" });
